7ccad5a6eea5d5e5867a2b29bebf805ca28b8db1
Vinod G <vinodgowda1998@gmail.com>
2023-06-07 20:46:19 +0530

    async,await

--- promise.js
+++ promise.js
@@ -1,3 +1,5 @@
+// Promise is a object representing the eventual (delay of) complition / failure of an asynchronous operation..
+
 var resultList = [
   { name: "MNO", result: "Pass" },
   { name: "ABC", result: "Fail" },
@@ -15,10 +17,12 @@ function checkResult(event) {
 }
 
 function getResult(sname) {
+  // Promise object takes a function with two args of callback function to represnt success / failure of an operation which is optional
   var promiseObject = new Promise((resolve, reject) => {
     for (const obj of resultList) {
       if (obj.name == sname) {
-        resolve("Result : ".concat(obj.result));
+        setTimeout(
+        resolve("Result : ".concat(obj.result)),5000)
       } else{
         const err= new Error("Student name is not valid")
         reject(err.message);
@@ -33,3 +37,16 @@ function printMessage(promiseObject) {
     console.log(promiseObject)
   document.getElementById("result").innerHTML = promiseObject;
 }
+
+// Async , Await 
+
+// async makes a function return promise .. to check remove async keyword -- throws error
+(async function abc(){ return "Hi, I'm returning a promise!!!!!!!!!!!"})().then(data=>console.log(data));
+
+// await makes a function to wait for a promise and doesn't move the next line of the code... to check remove await keyword
+(async function xyz(){
+  console.log('before promise');
+  const a= await getResult('MNO');
+  console.log(a);
+  console.log('after promise')
+})();
